ac39536f9611a48213564546091ad3456a151202
Ritvik Mishra <mishraritvik@gmail.com>
2015-04-13 16:01:05 -0700

    added basic counting code and keeping track of offsets for users

--- src/recommender.cpp
+++ src/recommender.cpp
@@ -20,11 +20,20 @@ MatrixXi read_data(int rows, int cols, string filename) {
 }
 
 Learner::Learner() {
-    // do nothing
+    double *avg_user_rating     = new double[NUM_USERS];
+    double *user_rating_count   = new double[NUM_USERS];
+    double *avg_user_date       = new double[NUM_USERS];
+
+    double *avg_movie_rating    = new double[NUM_MOVIES];
+    double *movie_ratings_count = new double[NUM_MOVIES];
 }
 
 Learner::~Learner() {
-    // do nothing
+    delete avg_user_rating;
+    delete user_rating_count;
+    delete avg_user_date;
+    delete avg_movie_rating;
+    delete movie_ratings_count;
 }
 
 void Learner::set_data(MatrixXi data) {
@@ -35,12 +44,50 @@ void Learner::set_qual(MatrixXi qual) {
     this->qual = qual;
 }
 
+void Learner::get_counts() {
+    unsigned int user, movie, rating, date;
+    for (unsigned int i = 0; i < DATA_SIZE; ++i) {
+        user   = this->data(i, USER_COL);
+        movie  = this->data(i, MOVIE_COL);
+        rating = this->data(i, RATING_COL);
+        date   = this->data(i, TIME_COL);
+
+        this->avg_rating += rating;
+
+        this->avg_user_rating[user] += rating;
+        this->avg_user_date[user] += date;
+        this->user_rating_count[user]++;
+
+        this->avg_movie_rating[movie] += rating;
+        this->user_ratings_count[movie]++;
+    }
+
+    this->avg_rating /= DATA_SIZE;
+
+    for (unsigned int i = 0; i < NUM_USERS; ++i) {
+        this->avg_user_rating[i] /= this->user_rating_count[i];
+        this->avg_user_date[i] /= this->user_rating_count[i];
+        this->user_rating_count[i] = pow(this->user_rating_count[i], -0.5);
+    }
+
+    for (unsigned int i = 0; i < NUM_MOVIES; ++i) {
+        this->avg_movie_rating[i] /= this->movie_rating_count[i];
+    }
+}
+
+void Learner::initialize() {
+
+}
+
+
 void Learner::train() {
     // nead to write this
 }
 
 void Learner::predict() {
-    // need to write this
+    for (unsigned int i = 0; i < QUAL_SIZE; ++i) {
+        // prediction function
+    }
 }
 
 int main() {
--- src/recommender.hpp
+++ src/recommender.hpp
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cmath>
+
 #include <iostream>
 #include <sstream>
 #include <fstream>
@@ -5,14 +9,23 @@
 #include <vector>
 #include "/usr/local/Cellar/eigen/3.2.3/include/eigen3/Eigen/Dense"
 
+// to change between um and mu these next four lines must be changed
 #define DATA_FILE  "data/mu/all.dta"
 #define QUAL_FILE  "data/mu/qual.dta"
+#define MOVIE_COL  0
+#define USER_COL   1
+#define TIME_COL   2
+#define RATING_COL 3
+
+// constants regarding data
 #define DATA_SIZE  102416306
 #define QUAL_SIZE  2749898
+#define NUM_USERS  458293
+#define NUM_MOVIES 17770
+
+// constants regarding learning
 #define NUM_FEATS  50
 #define NUM_ITERS  50
-#define NUM_USERS  458293 + 1
-#define NUM_MOVIES 17770 + 1
 
 using namespace std;
 using namespace Eigen;
@@ -33,6 +46,16 @@ private:
     MatrixXi data;
     MatrixXi qual;
     MatrixXf resp;
+
     MatrixXf U;
     MatrixXf V;
+
+    double avg_rating;
+
+    double[NUM_USERS] *avg_user_rating;
+    double[NUM_USERS] *user_rating_count;
+    double[NUM_USERS] *avg_user_date;
+
+    double[NUM_MOVIES] *avg_movie_rating;
+    double[NUM_MOVIES] *movie_ratings_count;
 };
